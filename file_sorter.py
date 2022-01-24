8c09d5c6594e8cf9288ed8017736a1ef92eaafea
WazWaz <wazwazman1@gmail.com>
2022-01-25 00:36:30 +0200

    modified:   .gitignore 	new file:   file_sorter.py

---
+++ file_sorter.py
@@ -0,0 +1,19 @@
+import os
+
+def get_file_types_list(path):
+    file_types = set()
+    file_list = os.listdir(path)
+
+    for file_name in file_list:
+        file_extention = file_name.split(".")[-1]
+        file_types.add("."+file_extention)
+
+    return(file_types)
+
+def test():
+    path = "C:/Users/Daniel/Desktop/100CANON"
+
+    print(get_file_types_list(path))
+
+if __name__ == "__main__" :
+    test()
