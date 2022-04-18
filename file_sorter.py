ea0c56d7d81654b1309ba95f82da92afde095999
WazWaz <wazwazman1@gmail.com>
2022-04-18 22:14:51 +0300

    modified:   file_sorter.py 	modified:   ui2.py 	deleted:    why.py

--- file_sorter.py
+++ file_sorter.py
@@ -2,8 +2,9 @@ from ui import Ui
 from rule import Rule
 from file import File
 from sorter import Sorter
-from ui2 import VariableArgument,FunctionArgument,Ui2
+from ui2 import VariableArgument, FunctionArgument, Ui2, MenuItem
 import os
+
 
 def default_update_function(files_moved, files_to_move):
     os.system("cls")
@@ -12,7 +13,9 @@ def default_update_function(files_moved, files_to_move):
     for i in range(dots):
         dot_string += "."
     print("moving file"+dot_string)
-    print(f'done {files_moved} of {files_to_move}, ({round(files_moved/files_to_move*1000)/10}%)')
+    print(
+        f'done {files_moved} of {files_to_move}, ({round(files_moved/files_to_move*1000)/10}%)')
+
 
 def logo_function():
     print(f'___________.__.__                     _________              __                ')
@@ -24,19 +27,32 @@ def logo_function():
     print()
     print()
 
+
+def update():
+    pass
+
+
 if __name__ == "__main__":
     # ui = Ui()
     # ui.start()
 
-    origin_folder = VariableArgument("origin folder","C:/Users/Daniel/Desktop/100CANON/JPG" ,1)
-    destination_folder = VariableArgument("destination folder","C:/Users/Daniel/Desktop/100CANON/JPG/1",2)
-    _rules = Rule.create_default_rules(Rule.get_file_types_list(origin_folder.arg_value), destination_folder.arg_value)
-    rules = VariableArgument("rules",_rules,3)
+    origin_folder = VariableArgument(
+        "origin folder", "C:/Users/Daniel/Desktop/100CANON/JPG", update, arg_place=1)
+    destination_folder = VariableArgument(
+        "destination folder", "C:/Users/Daniel/Desktop/100CANON/JPG/1", update, arg_place=2)
+    _rules = Rule.create_default_rules(Rule.get_file_types_list(
+        origin_folder.arg_value), destination_folder.arg_value)
+    rules = VariableArgument("rules", _rules, update, arg_place=3)
+
 
     file_list = File.create_file_list(origin_folder.arg_value)
-    sorter = Sorter(file_list,rules.arg_value,default_update_function)
-    copy = FunctionArgument("copy files",sorter.copy,4)
-    move = FunctionArgument("move files",sorter.copy,5)
+    sorter = Sorter(file_list, rules.arg_value, default_update_function)
+    copy = FunctionArgument("copy files", sorter.copy, arg_place=4)
+    move = FunctionArgument("move files", sorter.copy, arg_place=5)
 
-    ui = Ui2(origin_folder,destination_folder,rules,copy,move,logo_function=logo_function)
+    o_foler = MenuItem("origin folder",origin_folder, destination_folder, rules,
+             copy, move,arg_place=1)
+
+    ui = Ui2(o_foler, destination_folder, rules,
+             copy, move, logo_function=logo_function)
     ui.start()
--- ui2.py
+++ ui2.py
@@ -1,4 +1,6 @@
+from multiprocessing.connection import wait
 import os
+from time import sleep
 from tkinter import filedialog
 from tkinter import *
 import keyboard
@@ -20,44 +22,100 @@ class Ui2:
     def main_menu(self):
         os.system("cls")
         self.print_logo()
+        self.create_menu(self.args,"1")
+
+        # # draw menu
+        # item_number = 0
+        # for arg in self.args:
+        #     item_number += 1
+        #     if(item_number == arg.arg_place):
+        #         if(isinstance(arg, VariableArgument)):
+        #             print(str(arg.arg_place)+". set "+arg.arg_name)
+        #         elif(isinstance(arg, MenuItem)):
+        #             print(str(arg.menu_place)+". set "+arg.menu_name)
+        #         elif(isinstance(arg, FunctionArgument)):
+        #             print(str(arg.arg_place)+". "+arg.function_name)
+
+        # # wait for keypress and take action accordingly
+        # to_loop = True
+        # while to_loop:
+        #     key_pressed = keyboard.read_key()
+        #     if(key_pressed.isnumeric() and int(key_pressed) <= len(self.args) and int(key_pressed) > 0):
+        #         for arg in self.args:
+        #             if(int(key_pressed) == arg.arg_place):
+        #                 if(isinstance(arg, VariableArgument)):
+        #                     to_loop = False
+        #                     # open sub menu
+        #                     break
+        #                 elif(isinstance(arg, MenuItem)):
+        #                     to_loop = False
+        #                     break
+        #                 elif(isinstance(arg, FunctionArgument)):
+        #                     to_loop = False
+        #                     arg.function()
+        #                     break
+        # print()
+        # print("press any key To continue...")
+        # keyboard.read_key()
+        
+        # redraw menu
+        # self.main_menu()
+
+    def create_menu(self, args,a):
+        os.system("cls")
+        self.print_logo()
 
         # draw menu
         item_number = 0
-        for arg in self.args:
+        for arg in args:
             item_number += 1
             if(item_number == arg.arg_place):
                 if(isinstance(arg, VariableArgument)):
                     print(str(arg.arg_place)+". set "+arg.arg_name)
-                if(isinstance(arg, FunctionArgument)):
+                elif(isinstance(arg, MenuItem)):
+                    print(str(arg.arg_place)+". set "+arg.menu_name)
+                elif(isinstance(arg, FunctionArgument)):
                     print(str(arg.arg_place)+". "+arg.function_name)
 
         # wait for keypress and take action accordingly
         to_loop = True
+        sleep(10)
+        print("ready")
         while to_loop:
             key_pressed = keyboard.read_key()
-            if(key_pressed.isnumeric() and int(key_pressed) <= len(self.args) and int(key_pressed) > 0):
-                for arg in self.args:
+            print(key_pressed)
+            if(key_pressed.isnumeric() and int(key_pressed) <= len(args) and int(key_pressed) > 0):
+                for arg in args:
                     if(int(key_pressed) == arg.arg_place):
                         if(isinstance(arg, VariableArgument)):
                             to_loop = False
-                            # open sub menu
+                            print("a")
+                            arg.update_function()
                             break
-                        if(isinstance(arg, FunctionArgument)):
+                        elif(isinstance(arg, MenuItem)):
                             to_loop = False
+                            print("b")
+                            self.create_menu(arg.sub_menu_items,"2")
+                            break
+                        elif(isinstance(arg, FunctionArgument)):
+                            to_loop = False
+                            print("c")
                             arg.function()
                             break
         print()
-        print("press any key To continue...")
+        print(a,"press any key To continue...")
         keyboard.read_key()
-        
-        # redraw menu
-        self.main_menu()
 
-    def create_sub_menu(self):
-        os.system("cls")
+        return
 
     def update_var(self):
         pass
+
+class MenuItem:
+    def __init__(self,menu_name ,*sub_menu_items, arg_place=-1):
+        self.menu_name = menu_name
+        self.sub_menu_items = sub_menu_items
+        self.arg_place = arg_place
 
 
 class VariableArgument:
